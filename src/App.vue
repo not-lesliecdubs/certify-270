44799d2fc50b7ac3d5291b3ee4e0808b65a6a4de
Bryan Mikaelian <bryan@netlify.com>
2020-11-24 08:05:53 -0500

    Get everything working

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="container mx-auto m-20">
+      <div class="flex flex-col items-center">
+        <h1 class="text-8xl text-center pb-8">Certify 270</h1>
+        <p class="text-center max-w-prose pb-4">
+          After the election, each state will certify their election results and
+          confirm which electors are selected to vote in the Electoral College.
+          The law can differ between states but, in general, the winner of the
+          state's popular vote will be awarded the electors. Electors are
+          nominated by each respective party.
+        </p>
+        <p class="text-center max-w-prose">
+          Read more about the Electoral College on
+          <a
+            href="https://en.wikipedia.org/wiki/United_States_Electoral_College"
+            target="_blank"
+            class="text-blue-800"
+            >Wikipedia</a
+          >.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
